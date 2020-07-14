974d80805abf4fe525bff615eb3eb5b912638cb7
Clem <clement_392@hotmail.com>
2020-07-14 23:33:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# shopify-to-dhl-orders
+ This is to convert Shopify order export into DHL AP csv input format
---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Created by: Clement
+
+Shopify orders to DHL shipping format
+
+Will be able to let me export orders via Shopify and then convert it into the
+format that DHL portal can take in directly
+https://ecommerceportal.dhl.com/Portal/pages/login/userlogin.xhtml
+
+STEPS:
+1. Logistics team will just export the Shopify orders as csv
+2. Run the script with the shopify exported order list
+3. DHL formatted csv will be in an output folder
+(./output/dhl_<shopify_filename>.csv)
+"""
+
+
+def main():
+    """This function will serve as a starting point for the program"""
+    pass
+
+
+if __name__ == '__main__':
+    main()

191ae429ec5317109c21ff27f6afb524e41ceb10
Clem <clement_392@hotmail.com>
2020-07-15 00:27:14 +0800

    Getting ready

--- shopify_to_dhl_format.py
+++ shopify_to_dhl_format.py
@@ -14,15 +14,41 @@ STEPS:
 (./output/dhl_<shopify_filename>.csv)
 """
 
-import sys
-from account_details import *
-from strap_stats import *
-from shipping_service import *
+import csv
+import xlrd
+
+DHL_DELIMITER = '\t'
+SHOPIFY_DELIMITER = ','
+DHL_HEADERS_FILE = './resources/dhl_csv_headers.xlsx'
+
+
+def import_shopify_csv(path):
+    with open(path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=SHOPIFY_DELIMITER)
+        for row in csv_reader:
+            print(row)
+
+
+def get_dhl_csv_format(path):
+    workbook = xlrd.open_workbook(path)
+    sheet = workbook.sheet_by_index(0)
+
+    for row in range(sheet.nrows):
+        values = sheet.row_values(row)
+        print(values)
 
 
 def main():
     """This function will serve as a starting point for the program"""
-    print(PICKUPACCOUNTNUMBER)
+    try:
+        # shopify_csv = sys.argv[1]
+        shopify_csv = './resources/shopify_csv_headers.csv'
+        # import_shopify_csv(shopify_csv)
+
+        get_dhl_csv_format(DHL_HEADERS_FILE)
+
+    except IndexError:
+        print('Please enter Shopify order csv as parameter.')
 
 
 if __name__ == '__main__':

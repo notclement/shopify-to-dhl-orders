336d7ced2609a535055533581fa515b978c2d373
Clem <clement_392@hotmail.com>
2020-08-26 22:27:51 +0800

    WHAT?
    Each week, i solve a problem that didn't occur the week before..
    THEN WHY DID IT WORK LAST WEEK???

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 UPLOAD_FOLDER = r'site\uploads'
 OUTPUT_FOLDER = r'E:\Users\Clement\Documents\GitHub\shopify-to-dhl-orders\site\output'
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./site/templates')
 app.secret_key = "secretkey"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
